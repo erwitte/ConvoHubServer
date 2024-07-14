46e28faacee93e9542727d2d113d1cb8d0463a38
Erik Witte <witte.erik2@gmail.com>
2024-07-14 13:15:34 +0200

    did tests for registration

--- src/test/java/com/example/demo/RESTfulControllerTests.java
+++ src/test/java/com/example/demo/RESTfulControllerTests.java
@@ -9,17 +9,14 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultMatcher;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(RESTfulController.class)
